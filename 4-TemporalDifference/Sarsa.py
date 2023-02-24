d7188392242d9b42b65d409eddad6d6f5bc78835
ChenYangZuo <zuo-cy2008@163.com>
2023-02-24 12:35:46 +0800

    add Sarsa code

--- 4-TemporalDifference/Sarsa.py
+++ 4-TemporalDifference/Sarsa.py
@@ -34,7 +34,7 @@ class CliffWalkingEnv:
     def reset(self):
         self.x = 0
         self.y = self.nrow - 1
-        return self.y * self.nrow + self.x
+        return self.y * self.ncol + self.x
 
 
 class Sarsa:
@@ -48,11 +48,12 @@ class Sarsa:
     # epsilon-贪婪算法
     def take_action(self, state):
         if np.random.random() < self.epsilon:
-            action = np.random.randint(self.n_action)
+            action = np.random.randint(self.n_action)  # 随机选择动作增加探索
         else:
-            action = np.argmax(self.Q_table[state])
+            action = np.argmax(self.Q_table[state])  # 执行最优动作
         return action
 
+    # 用于打印策略
     def best_action(self, state):
         Q_max = np.max(self.Q_table[state])
         a = [0 for _ in range(self.n_action)]
@@ -61,9 +62,63 @@ class Sarsa:
                 a[i] = 1
         return a
 
+    # 更新Q表
     def update(self, s0, a0, r, s1, a1):
-        td_error = r + self.gamma * self.Q_table[s1, a1] - self.Q_table[s0, a0]
-        self.Q_table[s0, a0] += self.alpha * td_error
+        self.Q_table[s0, a0] = self.Q_table[s0, a0] + self.alpha * (
+                r + self.gamma * self.Q_table[s1, a1] - self.Q_table[s0, a0])
+
+
+class nstep_Sarsa:
+    """ n步Sarsa算法 """
+
+    def __init__(self, n, ncol, nrow, epsilon, alpha, gamma, n_action=4):
+        self.Q_table = np.zeros([nrow * ncol, n_action])
+        self.n_action = n_action
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.n = n  # 采用n步Sarsa算法
+        self.state_list = []  # 保存之前的状态
+        self.action_list = []  # 保存之前的动作
+        self.reward_list = []  # 保存之前的奖励
+
+    def take_action(self, state):
+        if np.random.random() < self.epsilon:
+            action = np.random.randint(self.n_action)
+        else:
+            action = np.argmax(self.Q_table[state])
+        return action
+
+    def best_action(self, state):  # 用于打印策略
+        Q_max = np.max(self.Q_table[state])
+        a = [0 for _ in range(self.n_action)]
+        for i in range(self.n_action):
+            if self.Q_table[state, i] == Q_max:
+                a[i] = 1
+        return a
+
+    def update(self, s0, a0, r, s1, a1, done):
+        self.state_list.append(s0)
+        self.action_list.append(a0)
+        self.reward_list.append(r)
+        if len(self.state_list) == self.n:  # 若保存的数据可以进行n步更新
+            G = self.Q_table[s1, a1]  # 得到Q(s_{t+n}, a_{t+n})
+            for i in reversed(range(self.n)):
+                G = self.gamma * G + self.reward_list[i]  # 不断向前计算每一步的回报
+                # 如果到达终止状态,最后几步虽然长度不够n步,也将其进行更新
+                if done and i > 0:
+                    s = self.state_list[i]
+                    a = self.action_list[i]
+                    self.Q_table[s, a] += self.alpha * (G - self.Q_table[s, a])
+            s = self.state_list.pop(0)  # 将需要更新的状态动作从列表中删除,下次不必更新
+            a = self.action_list.pop(0)
+            self.reward_list.pop(0)
+            # n步Sarsa的主要更新步骤
+            self.Q_table[s, a] += self.alpha * (G - self.Q_table[s, a])
+        if done:  # 如果到达终止状态,即将开始下一条序列,则将列表全清空
+            self.state_list = []
+            self.action_list = []
+            self.reward_list = []
 
 
 def print_agent(agent, env, action_meaning, disaster=[], end=[]):
@@ -83,43 +138,95 @@ def print_agent(agent, env, action_meaning, disaster=[], end=[]):
         print()
 
 
-ncol = 12
-nrow = 4
-env = CliffWalkingEnv(ncol, nrow)
-np.random.seed(0)
-epsilon = 0.1
-alpha = 0.1
-gamma = 0.9
-agent = Sarsa(ncol, nrow, epsilon, alpha, gamma)
-num_episodes = 500
+def one_step():
+    ncol = 12
+    nrow = 4
+    env = CliffWalkingEnv(ncol, nrow)
+    np.random.seed(0)
+    epsilon = 0.1
+    alpha = 0.1
+    gamma = 0.9
+    agent = Sarsa(ncol, nrow, epsilon, alpha, gamma)
+    num_episodes = 500
 
-return_list = []
-for i in range(10):  # 显示10个进度条
-    with tqdm(total=int(num_episodes / 10), desc="Iteration %d" % i) as pbar:
-        for i_episode in range(int(num_episodes / 10)):
-            episode_return = 0
-            state = env.reset()
-            action = agent.take_action(state)
-            done = False
-            while not done:
-                next_state, reward, done = env.step(action)
-                next_action = agent.take_action(next_state)
-                episode_return += reward
-                agent.update(state, action, reward, next_state, next_action)
-                state = next_state
-                action = next_action
-            return_list.append(episode_return)
-            if (i_episode + 1) % 10 == 0:
-                pbar.set_postfix({"episode": "%d" % (num_episodes / 10 * i + i_episode + 1),
-                                  "return": "%.3f" % np.mean(return_list[-10:])})
-            pbar.update(1)
+    return_list = []
+    for i in range(10):  # 显示10个进度条
+        with tqdm(total=int(num_episodes / 10), desc="Iteration %d" % i) as pbar:
+            for i_episode in range(int(num_episodes / 10)):
+                episode_return = 0
+                state = env.reset()
+                action = agent.take_action(state)
+                done = False
+                while not done:
+                    next_state, reward, done = env.step(action)
+                    next_action = agent.take_action(next_state)
+                    episode_return += reward
+                    agent.update(state, action, reward, next_state, next_action)
+                    state = next_state
+                    action = next_action
+                return_list.append(episode_return)
+                if (i_episode + 1) % 10 == 0:
+                    pbar.set_postfix({"episode": "%d" % (num_episodes / 10 * i + i_episode + 1),
+                                      "return": "%.3f" % np.mean(return_list[-10:])})
+                pbar.update(1)
 
-episodes_list = list(range(len(return_list)))
-plt.plot(episodes_list, return_list)
-plt.xlabel("Episodes")
-plt.ylabel("Returns")
-plt.title("Sarsa on CliffWalking")
-plt.show()
+    episodes_list = list(range(len(return_list)))
+    plt.plot(episodes_list, return_list)
+    plt.xlabel("Episodes")
+    plt.ylabel("Returns")
+    plt.title("Sarsa on CliffWalking")
+    plt.show()
 
-action_meaning = ["^", "v", "<", ">"]
-print_agent(agent, env, action_meaning, list(range(37, 47)), [47])
+    action_meaning = ["^", "v", "<", ">"]
+    print_agent(agent, env, action_meaning, list(range(37, 47)), [47])
+
+
+def n_step():
+    ncol = 12
+    nrow = 4
+    env = CliffWalkingEnv(ncol, nrow)
+    np.random.seed(0)
+    n_step = 5  # 5步Sarsa算法
+    alpha = 0.1
+    epsilon = 0.1
+    gamma = 0.9
+    agent = nstep_Sarsa(n_step, ncol, nrow, epsilon, alpha, gamma)
+    num_episodes = 500  # 智能体在环境中运行的序列的数量
+
+    return_list = []  # 记录每一条序列的回报
+    for i in range(10):  # 显示10个进度条
+        # tqdm的进度条功能
+        with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes / 10)):  # 每个进度条的序列数
+                episode_return = 0
+                state = env.reset()
+                action = agent.take_action(state)
+                done = False
+                while not done:
+                    next_state, reward, done = env.step(action)
+                    next_action = agent.take_action(next_state)
+                    episode_return += reward  # 这里回报的计算不进行折扣因子衰减
+                    agent.update(state, action, reward, next_state, next_action,
+                                 done)
+                    state = next_state
+                    action = next_action
+                return_list.append(episode_return)
+                if (i_episode + 1) % 10 == 0:  # 每10条序列打印一下这10条序列的平均回报
+                    pbar.set_postfix({
+                        'episode':
+                            '%d' % (num_episodes / 10 * i + i_episode + 1),
+                        'return':
+                            '%.3f' % np.mean(return_list[-10:])
+                    })
+                pbar.update(1)
+
+    episodes_list = list(range(len(return_list)))
+    plt.plot(episodes_list, return_list)
+    plt.xlabel('Episodes')
+    plt.ylabel('Returns')
+    plt.title('5-step Sarsa on {}'.format('Cliff Walking'))
+    plt.show()
+
+
+if __name__ == '__main__':
+    n_step()

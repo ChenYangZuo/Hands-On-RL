cc0220ebad48ee677d2294be23114fceeedf5d6f
ChenYangZuo <zuo-cy2008@163.com>
2023-03-05 19:22:33 +0800

    change

--- 6-DQN/DQN.py
+++ 6-DQN/DQN.py
@@ -4,6 +4,7 @@
 # @Author : ZZZCY
 # @File : [Hands-On-RL] --> DQN.py
 
+import os
 import random
 import gym
 import numpy as np
@@ -13,7 +14,6 @@ import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import rl_utils
-
 
 class ReplayBuffer:
     """ 经验回放池 """
@@ -86,10 +86,19 @@ class DQN:
         if self.count % self.target_update == 0:
             self.target_q_net.load_state_dict(self.q_net.state_dict())  # 使用Q网络更新目标网络
         self.count += 1
+    
+    def load(self):
+        self.q_net.load_state_dict(torch.load("./save_mode/model.pth"))
+    
+    def save(self):
+        folder = "./save_mode"
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        torch.save(self.q_net.state_dict(), folder + "/model.pth")
 
 
 lr = 2e-3
-num_episodes = 500
+num_episodes = 300
 hidden_dim = 128
 gamma = 0.98
 epsilon = 0.01
@@ -110,50 +119,71 @@ state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.n
 agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epsilon, target_update, device)
 
-return_list = []
-for i in range(10):
-    with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
-        for i_episode in range(int(num_episodes / 10)):
-            episode_return = 0
+def train():
+    return_list = []
+    for i in range(10):
+        with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes / 10)):
+                episode_return = 0
+                state = env.reset()
+                done = False
+                while not done:
+                    action = agent.take_action(state)
+                    next_state, reward, done, _ = env.step(action)
+                    replay_buffer.add(state, action, reward, next_state, done)
+                    state = next_state
+                    episode_return += reward
+                    # 当buffer数据的数量超过一定值后,才进行Q网络训练
+                    if replay_buffer.size() > minimal_size:
+                        b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
+                        transition_dict = {
+                            'states': b_s,
+                            'actions': b_a,
+                            'next_states': b_ns,
+                            'rewards': b_r,
+                            'dones': b_d
+                        }
+                        agent.update(transition_dict)
+                return_list.append(episode_return)
+                if (i_episode + 1) % 10 == 0:
+                    pbar.set_postfix({
+                        'episode':
+                            '%d' % (num_episodes / 10 * i + i_episode + 1),
+                        'return':
+                            '%.3f' % np.mean(return_list[-10:])
+                    })
+                pbar.update(1)
+    agent.save()
+
+    episodes_list = list(range(len(return_list)))
+    plt.plot(episodes_list, return_list)
+    plt.xlabel('Episodes')
+    plt.ylabel('Returns')
+    plt.title('DQN on {}'.format(env_name))
+    plt.show()
+
+    mv_return = rl_utils.moving_average(return_list, 9)
+    plt.plot(episodes_list, mv_return)
+    plt.xlabel('Episodes')
+    plt.ylabel('Returns')
+    plt.title('DQN on {}'.format(env_name))
+    plt.show()
+
+def test():
+    time = 0
+    agent.load()
+    state = env.reset()
+    for i in range(1000):
+        env.render()
+        action = agent.take_action(state)
+        next_state, reward, done, _ = env.step(action)
+        if done:
             state = env.reset()
-            done = False
-            while not done:
-                action = agent.take_action(state)
-                next_state, reward, done, _ = env.step(action)
-                replay_buffer.add(state, action, reward, next_state, done)
-                state = next_state
-                episode_return += reward
-                # 当buffer数据的数量超过一定值后,才进行Q网络训练
-                if replay_buffer.size() > minimal_size:
-                    b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
-                    transition_dict = {
-                        'states': b_s,
-                        'actions': b_a,
-                        'next_states': b_ns,
-                        'rewards': b_r,
-                        'dones': b_d
-                    }
-                    agent.update(transition_dict)
-            return_list.append(episode_return)
-            if (i_episode + 1) % 10 == 0:
-                pbar.set_postfix({
-                    'episode':
-                        '%d' % (num_episodes / 10 * i + i_episode + 1),
-                    'return':
-                        '%.3f' % np.mean(return_list[-10:])
-                })
-            pbar.update(1)
+            print(f"[{time}] Reset")
+            time += 1
+    env.close()
 
-episodes_list = list(range(len(return_list)))
-plt.plot(episodes_list, return_list)
-plt.xlabel('Episodes')
-plt.ylabel('Returns')
-plt.title('DQN on {}'.format(env_name))
-plt.show()
-
-mv_return = rl_utils.moving_average(return_list, 9)
-plt.plot(episodes_list, mv_return)
-plt.xlabel('Episodes')
-plt.ylabel('Returns')
-plt.title('DQN on {}'.format(env_name))
-plt.show()
+if __name__ == "__main__":
+    print("Start!")
+    # train()
+    test()
